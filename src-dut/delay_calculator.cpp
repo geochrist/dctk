7f95f1b5863b65911f4fbcbf4f0f35ac52f6c83d
George Chen <george@geochrist.com>
2019-10-13 22:04:54 -0700

    Add files via upload

---
+++ src-dut/delay_calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "dctk.hpp"
+#include "CellLib.hpp"
+#include "Circuit.hpp"
+
+int compute_delays(dctk::CellLib* cell_lib, dctk::CircuitPtrVec* circuitMgr) {
+
+    std::cout << "Computing Delays for " << circuitMgr->size()
+	      << " circuits." << std::endl;
+
+
+    return 0;
+
+}
---
+++ src-dut/delay_calculator.hpp
@@ -0,0 +1,8 @@
+#ifndef DELAY_CALCULATOR_HPP_
+#define DELAY_CALCULATOR_HPP_
+
+#include "dctk.hpp"
+
+int compute_delays(dctk::CellLib* cell_lib, dctk::CircuitPtrVec* circuitMgr);
+
+#endif
