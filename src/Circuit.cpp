423fe72704a875859c437e5bc6c50c4911a6f564
George Chen <george@geochrist.com>
2019-10-19 23:36:35 -0700

    updated to work with PiModel

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -6,12 +6,16 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include <src/Circuit.hpp>
 
 namespace dctk {
 
 Circuit::Circuit(const std::string& s) {
-    this->_name = s;
+    _name = s;
+    _delay = 0.0;
+    _slew = 0.0;
 }
 
 Circuit& Circuit::set_input_voltage_source(const std::string& s) {
@@ -25,17 +29,36 @@ Circuit& Circuit::set_driver(const std::string& s) {
 }
 
 Circuit& Circuit::set_driver_interconnect(const std::string& s) {
-    this->_driver_interconnect = s;
+
+    // split string into 3 tokens and assign to cnear, res, and cfar
+ 
+    std::istringstream iss(s);
+    std::vector<std::string> results((std::istream_iterator<std::string>(iss)),
+				     std::istream_iterator<std::string>());
+
+    // there should be exactly 3 tokens
+    this->_driver_interconnect.set_cnear(stof(results[0]));
+    this->_driver_interconnect.set_res(stof(results[1]));
+    this->_driver_interconnect.set_cfar(stof(results[2]));
     return *this;
 }
 
 Circuit& Circuit::set_load(const std::string& s) {
+
     this->_load = s;
     return *this;
 }
 
 Circuit& Circuit::set_load_interconnect(const std::string& s) {
-    this->_load_interconnect = s;
+
+    std::istringstream iss(s);
+    std::vector<std::string> results((std::istream_iterator<std::string>(iss)),
+				     std::istream_iterator<std::string>());
+
+    // there should be exactly 3 tokens
+    this->_load_interconnect.set_cnear(stof(results[0]));
+    this->_load_interconnect.set_res(stof(results[1]));
+    this->_load_interconnect.set_cfar(stof(results[2]));
     return *this;
 }
 
@@ -43,9 +66,17 @@ void Circuit::dump() {
     std::cout << "Name = " << this->_name << std::endl;
     std::cout << "Voltage Source = " << this->_input_voltage_source << std::endl;
     std::cout << "Driver = " << this->_driver << std::endl;
-    std::cout << "Driver Interconnect = " << this->_driver_interconnect << std::endl;
+    std::cout << "Driver Interconnect = " << this->_driver_interconnect.get_cnear()
+	      << " " << this->_driver_interconnect.get_res()
+	      << " " << this->_driver_interconnect.get_cfar()
+	      << std::endl;
     std::cout << "Load = " << this->_load << std::endl;
-    std::cout << "load_interconnect = " << this->_load_interconnect << std::endl;
+    std::cout << "load_interconnect = " << this->_load_interconnect.get_cnear()
+	      << " " << this->_load_interconnect.get_res()
+	      << " " << this->_load_interconnect.get_cfar()
+	      << std::endl;
+    std::cout << "delay = " << this->_delay << std::endl;
+    std::cout << "slew = " << this->_slew << std::endl;
     std::cout << "----------" << std::endl;
 }
 
--- src/Circuit.hpp
+++ src/Circuit.hpp
@@ -2,6 +2,7 @@
 #define DCTK_CIRCUIT_HPP_
 
 #include <string>
+#include "PiModel.hpp"
 
 namespace dctk {
 
@@ -17,15 +18,27 @@ public:
     Circuit& set_load_interconnect(const std::string&);
     void dump();
 
+    // return values
+    void set_slew(float f) { _slew = f; }
+    void set_delay(float f) { _delay = f; }
+    float get_slew(float f) { return _slew; }
+    float get_delay(float f) { return _delay; }
+    
+
 
 private:
 
     std::string _name;
     std::string _input_voltage_source;
     std::string _driver;
-    std::string _driver_interconnect;
+    PiModel _driver_interconnect;
     std::string _load;
-    std::string _load_interconnect;
+    PiModel _load_interconnect;
+
+    // return values
+    float _delay;
+    float _slew;
+
 
 };
 
